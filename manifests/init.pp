8201cb02383e1a6eb8d641dec6fed7b559803e05
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-11-26 10:59:15 +0100

    oh-my-zsh personal theme

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,4 +10,11 @@ class ohmyzsh {
     source => 'robbyrussell/oh-my-zsh',
     path   => "${home}/.oh-my-zsh"
   }
+
+  file { "${home}/.oh-my-zsh/themes/stein_inge.zsh-theme":
+        mode => "0644",
+        owner => '${::boxen_user}',
+        group => 'staff',
+        source => 'puppet:///modules/ohmyzsh/stein_inge.zsh-theme',
+    }
 }
